b528a1bc3b6f8adc52c4619ed08671735827d1f3
DMon777 <bessalov88@mail.ru>
2018-05-29 17:44:04 +0300

    init

---
+++ src/StrMod.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Dmon777\StringModifier;
+
+
+class StrMod
+{
+
+    /**
+     * @param string $str
+     * @return string
+     */
+    public static function UP(string $str)
+    {
+        return strtoupper($str);
+    }
+
+    /**
+     * @param string $str
+     * @return string
+     */
+    public static function LOW(string $str)
+    {
+        return strtolower($str);
+    }
+
+    /**
+     * @param $str
+     * @return string
+     */
+    public static function TRANSLITERATOR(string $str)
+    {
+        $rus = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'];
+        $lat = ['A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya'];
+        return str_replace($rus, $lat, $str);
+    }
+
+
+
+
+
+}
